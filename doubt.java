5fa22de8202be2d1f6f509a6d812f0359b5dc09d
Kishoreram2004 <kishoreram10106@gmail.com>
2026-07-10 18:36:21 +0530

    day 5 javascript practice

---
+++ doubt.java
@@ -0,0 +1,12 @@
+public class doubt {
+    public static void main(String[] args) {
+        int i = 1;
+        if(i==1){
+            System.out.println("1if");
+        }else if(i==1){
+            System.out.println("1elsiff");
+        }else{
+            System.out.println("else");
+        }
+    }
+}
